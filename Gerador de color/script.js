1163298e89900a94965b1cdbb8a6ed4efd7f5714
juancosta0 <121205013+juancosta0@users.noreply.github.com>
2023-02-27 02:31:13 -0300

    Desafio css 01

--- Gerador de color/script.js
+++
@@ -1,9 +0,0 @@
-function gerar(){
-    var red=parseInt(Math.random()*(256-1)+ 1)
-    var Green=parseInt(Math.random()*(256-1)+ 1)
-    var blue=parseInt(Math.random()*(256-1)+ 1)
-    document.getElementById("container").style=`background-color: rgb(${red}, ${Green}, ${blue})`
-    alert(`rgb(${red}, ${Green}, ${blue})`)
-    
-}
-
